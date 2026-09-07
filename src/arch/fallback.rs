c6c1d1130107745b504922d4fb2d9843e81d0609
chiri <chirizxc@proton.me>
2026-09-07 05:19:18 +0300

    fix

--- src/arch/fallback.rs
+++ src/arch/fallback.rs
@@ -4,6 +4,22 @@
 use super::table::CRC32C_TABLE;
 
 #[inline]
+#[target_feature(enable = "sse")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+unsafe fn prefetch(ptr: *const u8) {
+    #[cfg(target_arch = "x86")]
+    use core::arch::x86::{_MM_HINT_T0, _mm_prefetch};
+    #[cfg(target_arch = "x86_64")]
+    use core::arch::x86_64::{_MM_HINT_T0, _mm_prefetch};
+
+    _mm_prefetch::<_MM_HINT_T0>(ptr.cast::<i8>());
+}
+
+#[inline(always)]
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+const unsafe fn prefetch(_: *const u8) {}
+
+#[inline]
 pub fn crc32c(crc0: u32, buf: &[u8], len: usize) -> u32 {
     const UNROLL: usize = 4;
     const BYTES_AT_ONCE: usize = 16 * UNROLL;
@@ -19,7 +35,7 @@ pub fn crc32c(crc0: u32, buf: &[u8], len: usize) -> u32 {
         // SAFETY: length >= 320, so PREFETCH_AHEAD bytes are
         // within the original buffer.
         unsafe {
-            core::intrinsics::prefetch_read_data::<_, 3>(current.cast::<u8>().add(PREFETCH_AHEAD));
+            prefetch(current.cast::<u8>().add(PREFETCH_AHEAD));
         }
 
         for _ in 0..UNROLL {
