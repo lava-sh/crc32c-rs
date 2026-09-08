a3de4358a314342d7a66b04f632c812781cf9e64
chiri <chirizxc@proton.me>
2026-09-08 04:46:34 +0300

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import importlib.util
 import logging
 from collections.abc import Callable
 
@@ -10,18 +9,17 @@ logger = logging.getLogger(__name__)
 
 
 def _import(module_name: str, pattern: str) -> dict[str, Callable]:
-    spec = importlib.util.find_spec(module_name)
-    if spec is None:
-        return {}
+    impls = {}
     try:
         module = __import__(module_name, fromlist=["*"])
-        return {
+        impls = {
             name: getattr(module, name)
             for name in dir(module)
             if name.startswith(pattern)
         }
     except ImportError:
-        return {}
+        pass
+    return impls
 
 
 @pytest.fixture(scope="session")
@@ -38,18 +36,18 @@ def crc_impl() -> list[tuple[str, Callable[..., int]]]:
     impls_ = _import("crc32c_rs", "crc32c_")
 
     requirements = {
-        "avx512vl_vpclmulqdq_v3s1_s3": {"avx512f", "avx512vl", "vpclmulqdq"},
-        "avx512vl_vpclmulqdq_v3s2x4": {"avx512f", "avx512vl", "vpclmulqdq"},
-        "avx512vl_vpclmulqdq_v4s5x3": {"avx512f", "avx512vl", "vpclmulqdq"},
-        "avx512vl_pclmulqdq_v9s3x4e": {"avx512vl", "pclmulqdq"},
-        "sse42_pclmulqdq_v1s3x2": {"sse4_2", "pclmulqdq"},
-        "sse42_pclmulqdq_v1s3x3": {"sse4_2", "pclmulqdq"},
-        "sse42_pclmulqdq_v1s4x2": {"sse4_2", "pclmulqdq"},
-        "sse42_pclmulqdq_v7s3x3": {"sse4_2", "pclmulqdq"},
-        "sse42_pclmulqdq_v8s3x3": {"sse4_2", "pclmulqdq"},
-        "aes_crc_v12e_v1": {"aes", "crc32"},
-        "aes_v3s4x2e_v2": {"aes", "crc32"},
-        "aes_sha3_v9s3x2e_s3": {"aes", "crc32", "sha3"},
+        "crc32c_avx512vl_vpclmulqdq_v3s1_s3": {"avx512f", "avx512vl", "vpclmulqdq"},
+        "crc32c_avx512vl_vpclmulqdq_v3s2x4": {"avx512f", "avx512vl", "vpclmulqdq"},
+        "crc32c_avx512vl_vpclmulqdq_v4s5x3": {"avx512f", "avx512vl", "vpclmulqdq"},
+        "crc32c_avx512vl_pclmulqdq_v9s3x4e": {"avx512vl", "pclmulqdq"},
+        "crc32c_sse42_pclmulqdq_v1s3x2": {"sse4_2", "pclmulqdq"},
+        "crc32c_sse42_pclmulqdq_v1s3x3": {"sse4_2", "pclmulqdq"},
+        "crc32c_sse42_pclmulqdq_v1s4x2": {"sse4_2", "pclmulqdq"},
+        "crc32c_sse42_pclmulqdq_v7s3x3": {"sse4_2", "pclmulqdq"},
+        "crc32c_sse42_pclmulqdq_v8s3x3": {"sse4_2", "pclmulqdq"},
+        "crc32c_aes_crc_v12e_v1": {"aes", "crc32"},
+        "crc32c_aes_v3s4x2e_v2": {"aes", "crc32"},
+        "crc32c_aes_sha3_v9s3x2e_s3": {"aes", "crc32", "sha3"},
     }
 
     impls = []
@@ -60,7 +58,7 @@ def crc_impl() -> list[tuple[str, Callable[..., int]]]:
             impls.append((name, implementation))
             logger.info("crc32c_rs.%s available", name)
 
-    impls.append(("fallback", crc32c_fallback))
+    impls.append(("crc32c_fallback", crc32c_fallback))
     logger.info("")
     logger.info(
         "Implementations: %s",
