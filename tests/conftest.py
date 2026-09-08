1e5a4fb7218f6c3ea65c96856ed6dbac1c84e998
chiri <chirizxc@proton.me>
2026-09-08 04:41:16 +0300

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import importlib.util
 import logging
 from collections.abc import Callable
 
@@ -5,54 +6,22 @@ import archspec.cpu
 import pytest
 from crc32c_rs import crc32c_fallback
 
-from .types import CrcFn
-
 logger = logging.getLogger(__name__)
 
-x86_impls: dict[str, CrcFn] = {}
-arm_impls: dict[str, CrcFn] = {}
 
-try:
-    from crc32c_rs import (
-        crc32c_avx512vl_pclmulqdq_v9s3x4e,
-        crc32c_avx512vl_vpclmulqdq_v3s1_s3,
-        crc32c_avx512vl_vpclmulqdq_v3s2x4,
-        crc32c_avx512vl_vpclmulqdq_v4s5x3,
-        crc32c_sse42_pclmulqdq_v1s3x2,
-        crc32c_sse42_pclmulqdq_v1s3x3,
-        crc32c_sse42_pclmulqdq_v1s4x2,
-        crc32c_sse42_pclmulqdq_v7s3x3,
-        crc32c_sse42_pclmulqdq_v8s3x3,
-    )
-except ImportError:
-    pass
-else:
-    x86_impls = {
-        "avx512vl_pclmulqdq_v9s3x4e": crc32c_avx512vl_pclmulqdq_v9s3x4e,
-        "avx512vl_vpclmulqdq_v3s1_s3": crc32c_avx512vl_vpclmulqdq_v3s1_s3,
-        "avx512vl_vpclmulqdq_v3s2x4": crc32c_avx512vl_vpclmulqdq_v3s2x4,
-        "avx512vl_vpclmulqdq_v4s5x3": crc32c_avx512vl_vpclmulqdq_v4s5x3,
-        "sse42_pclmulqdq_v1s3x2": crc32c_sse42_pclmulqdq_v1s3x2,
-        "sse42_pclmulqdq_v1s3x3": crc32c_sse42_pclmulqdq_v1s3x3,
-        "sse42_pclmulqdq_v1s4x2": crc32c_sse42_pclmulqdq_v1s4x2,
-        "sse42_pclmulqdq_v7s3x3": crc32c_sse42_pclmulqdq_v7s3x3,
-        "sse42_pclmulqdq_v8s3x3": crc32c_sse42_pclmulqdq_v8s3x3,
-    }
-
-try:
-    from crc32c_rs import (
-        crc32c_aes_crc_v12e_v1,
-        crc32c_aes_sha3_v9s3x2e_s3,
-        crc32c_aes_v3s4x2e_v2,
-    )
-except ImportError:
-    pass
-else:
-    arm_impls = {
-        "aes_crc_v12e_v1": crc32c_aes_crc_v12e_v1,
-        "aes_v3s4x2e_v2": crc32c_aes_v3s4x2e_v2,
-        "aes_sha3_v9s3x2e_s3": crc32c_aes_sha3_v9s3x2e_s3,
-    }
+def _import(module_name: str, pattern: str) -> dict[str, Callable]:
+    spec = importlib.util.find_spec(module_name)
+    if spec is None:
+        return {}
+    try:
+        module = __import__(module_name, fromlist=["*"])
+        return {
+            name: getattr(module, name)
+            for name in dir(module)
+            if name.startswith(pattern)
+        }
+    except ImportError:
+        return {}
 
 
 @pytest.fixture(scope="session")
@@ -66,9 +35,9 @@ def crc_impl() -> list[tuple[str, Callable[..., int]]]:
     logger.info("Features: %s", " ".join(sorted(features)))
     logger.info("")
 
-    impls: list[tuple[str, CrcFn]] = []
+    impls_ = _import("crc32c_rs", "crc32c_")
 
-    x86_requirements = {
+    requirements = {
         "avx512vl_vpclmulqdq_v3s1_s3": {"avx512f", "avx512vl", "vpclmulqdq"},
         "avx512vl_vpclmulqdq_v3s2x4": {"avx512f", "avx512vl", "vpclmulqdq"},
         "avx512vl_vpclmulqdq_v4s5x3": {"avx512f", "avx512vl", "vpclmulqdq"},
@@ -78,16 +47,15 @@ def crc_impl() -> list[tuple[str, Callable[..., int]]]:
         "sse42_pclmulqdq_v1s4x2": {"sse4_2", "pclmulqdq"},
         "sse42_pclmulqdq_v7s3x3": {"sse4_2", "pclmulqdq"},
         "sse42_pclmulqdq_v8s3x3": {"sse4_2", "pclmulqdq"},
+        "aes_crc_v12e_v1": {"aes", "crc32"},
+        "aes_v3s4x2e_v2": {"aes", "crc32"},
+        "aes_sha3_v9s3x2e_s3": {"aes", "crc32", "sha3"},
     }
-    for name, required in x86_requirements.items():
-        implementation = x86_impls.get(name)
-        if implementation is not None and required.issubset(features):
-            impls.append((name, implementation))
-            logger.info("crc32c_rs.%s available", name)
 
-    arm_requirements = {name: {"aes", "crc32"} for name in arm_impls}
-    for name, required in arm_requirements.items():
-        implementation = arm_impls.get(name)
+    impls = []
+
+    for name, required in requirements.items():
+        implementation = impls_.get(name)
         if implementation is not None and required.issubset(features):
             impls.append((name, implementation))
             logger.info("crc32c_rs.%s available", name)
