322cee5b0e2259ff09812388af2a867ce2053c3d
chiri <chirizxc@proton.me>
2026-09-15 16:10:46 +0300

    Update avx512vl_pclmulqdq_v9s3x4e.rs

--- src/arch/avx512vl_pclmulqdq_v9s3x4e.rs
+++ src/arch/avx512vl_pclmulqdq_v9s3x4e.rs
@@ -153,6 +153,11 @@ unsafe fn crc32c_small(mut crc0: u32, mut buf: *const u8, mut len: usize) -> u32
 #[target_feature(enable = "avx512vl,pclmulqdq")]
 pub unsafe fn crc32c(mut crc0: u32, mut buf: *const u8, mut len: usize) -> u32 {
     crc0 = !crc0;
+
+    if (32..=1024).contains(&len) {
+        return !unsafe { crc32c_small(crc0, buf, len) };
+    }
+
     while len != 0 && (buf as usize & 7) != 0 {
         crc0 = _mm_crc32_u8(crc0, unsafe { *buf });
         buf = unsafe { buf.add(1) };
@@ -162,9 +167,6 @@ pub unsafe fn crc32c(mut crc0: u32, mut buf: *const u8, mut len: usize) -> u32 {
         crc0 = crc32_u64(crc0, unsafe { buf.cast::<u64>().read_unaligned() });
         buf = unsafe { buf.add(8) };
         len -= 8;
-    }
-    if (32..=1024).contains(&len) {
-        return !unsafe { crc32c_small(crc0, buf, len) };
     }
     if len >= 240 {
         let blk = len / 240;
