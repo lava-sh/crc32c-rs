0efcf979633df09ad0c1d364a2124dc84fcaac05
chiri <chirizxc@proton.me>
2026-09-23 03:42:19 +0300

    add `__doc__` for `UnsupportedCPUFeatureError` (#86)

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,3 +1,8 @@
 use pyo3::{create_exception, exceptions::PyException};
 
-create_exception!(_crc32c_rs, UnsupportedCPUFeatureError, PyException);
+create_exception!(
+    _crc32c_rs,
+    UnsupportedCPUFeatureError,
+    PyException,
+    "Raised when the current CPU does not support required instructions."
+);
