07a301ade4d4f9b5e1b0596acb2872c154ef74dc
chiri <chirizxc@proton.me>
2026-09-25 03:31:20 +0300

    Update exceptions.rs

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,8 +1,8 @@
-use pyo3::{create_exception, exceptions::PyException};
+use pyo3::{create_exception, exceptions::PyRuntimeException};
 
 create_exception!(
     _crc32c_rs,
     UnsupportedCPUFeatureError,
-    PyException,
+    PyRuntimeException,
     "Raised when the current CPU does not support required instructions."
 );
